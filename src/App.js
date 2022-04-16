5e97ca11aacda90aae2aec509869e4f6a1887947
haitrieu <noname.r0202@gmail.com>
2022-04-16 22:03:29 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { STAFFS } from './shared/staffs';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h4>
+          Ứng dụng quản lý nhân sự v1.0
+        </h4>
       </header>
     </div>
   );
---
+++ src/components/StaffListComponent.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+
+class StaffList extends Component {
+    
+}
