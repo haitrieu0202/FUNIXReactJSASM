1ecf84eeee6c00b5bf4f809931b5d42a57f9f920
haitrieu <noname.r0202@gmail.com>
2022-04-18 21:14:40 +0700

    complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import StaffList from './components/StaffListComponent'
-
+import { Navbar, NavbarBrand } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h4>
-          Ứng dụng quản lý nhân sự v1.0
-        </h4>
-        <StaffList />
-      </header>
+      <Navbar dark color="primary" className="nav01">
+        <div className="container">
+          <NavbarBrand><h3>Ứng dụng quản lý nhân sự v1.0</h3></NavbarBrand>
+        </div>
+      </Navbar>
+      <StaffList /> 
     </div>
   );
 }
--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -8,7 +8,8 @@ class StaffList extends Component {
         super(props);
         this.StaffList = STAFFS;
         this.state = {
-            selectedStaff : null
+            selectedStaff : null,
+            columDefault: "divfull col-lg-4 col-md-6"
         }
     }
     clickStaff(infoStaff) {
@@ -16,10 +17,25 @@ class StaffList extends Component {
             selectedStaff : infoStaff
         })
     }
+    selectedCol2() {
+        this.setState({
+        columDefault: "divfull col-6"
+        })
+    }
+    selectedCol3() {
+        this.setState({
+        columDefault: "divfull col-4"
+        })
+    }
+    selectedCol6() {
+        this.setState({
+        columDefault: "divfull col-2"
+        })
+    }
     renderInfo(selectedStaff) {
         if (selectedStaff != null) {
             return(
-                <div>
+                <div className='staffCard col-lg-7 col-md-8'>
                     <Card>
                         <CardImg top width="100%" src={selectedStaff.image} alt={selectedStaff.name} />
                         <CardBody>
@@ -38,12 +54,21 @@ class StaffList extends Component {
     render() {
         const staffPost = this.StaffList.map((props) => {
             return (
-                <CardTitle onClick={() => this.clickStaff(props)} key={props.id}>{props.name}</CardTitle>
+                <div className={this.state.columDefault}>
+                    <Card className="oneStaff">
+                        <CardTitle onClick={() => this.clickStaff(props)} key={props.id}>{props.name}</CardTitle>
+                    </Card>
+                </div>
             )
         })
         return (
             <div>
-                {staffPost}
+                <div className='colSelected'>
+                    <button className='btn btn-warning' onClick={() => this.selectedCol2()}>2 cột</button>
+                    <button className='col3 btn btn-warning'onClick={() => this.selectedCol3()}>3 cột</button>
+                    <button className='btn btn-warning'onClick={() => this.selectedCol6()}>6 cột</button>
+                </div>
+                <div>{staffPost}</div><br></br>
                 <div>{this.renderInfo(this.state.selectedStaff)}</div>
             </div>
         )
