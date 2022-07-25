28e9101616a593395a7b31b66c017dbe3e50d34f
haitrieu <noname.r0202@gmail.com>
2022-07-25 22:14:20 +0700

    Asm3 Redux

---
+++ src/components/Salary.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import {Breadcrumb, BreadcrumbItem} from "reactstrap";
+import { Card, CardTitle } from 'reactstrap';
+
+function Salary(props) {
+    const arraysort = props.salary.sort(function(staff1, staff2){
+        return staff1.id - staff2.id;
+    });
+    const money = arraysort.map((staff) => {
+        return (
+            <div className="col-lg-4 col-md-6 col-sm-12">
+                <Card key={staff.id} className="marginbot">
+                    <CardTitle><b>{staff.name}</b></CardTitle>
+                    <CardTitle>Mã nhân viên: {staff.id}</CardTitle>
+                    <CardTitle>Hệ số lương: {staff.salaryScale}</CardTitle>
+                    <CardTitle>Số ngày làm thêm: {staff.overTime}</CardTitle>
+                    <CardTitle className='salary'>Lương: {(staff.salaryScale*3000000 + staff.overTime*200000).toFixed(0)}</CardTitle>
+                </Card>
+            </div>
+        )
+    })
+    return (
+        <div>
+            <div>
+                <Breadcrumb>
+                    <BreadcrumbItem>
+                        <Link to='/staffs'>Nhân Viên</Link>
+                    </BreadcrumbItem>
+                    <BreadcrumbItem active>Bảng Lương</BreadcrumbItem>
+                </Breadcrumb>
+            </div>
+            <Link to='/salary/sort'>
+                <button type="button" className="btn btn-info">Sắp xếp theo mức lương</button>    
+            </Link>               
+            <div className='post row'>{money}</div>
+        </div>
+    )    
+}
+
+export default Salary;
